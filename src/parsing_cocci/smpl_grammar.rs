2a41b3bebcf309d7ca73a1b657ab6ab46dbfb1f7
T Roy <t.roy@student.vu.nl>
2024-11-16 02:08:39 +0100

    save pt

--- src/parsing_cocci/smpl_grammar.rs
+++ src/parsing_cocci/smpl_grammar.rs
@@ -1,2 +1,14 @@
-pub type SpatchType<'a> = (&'a str, Vec<usize>);
-pub type RuleType<'a> = (&'a str, SpatchType<'a>);
+pub type ModType = (String, Vec<usize>);
+pub type RuleInfo<'a> = &'a str;//rulename as of now
+pub type RuleType<'a> = (RuleInfo<'a>, Metavars<'a>, ModType);
+pub type Metavar<'a> = (&'a str, Vec<&'a str>);
+pub type Metavars<'a> = Vec<Metavar<'a>>;
+
+pub fn make_metavar<'a>(t: &'a str, def: Vec<&'a str>) -> Metavar<'a> {
+    return (t, def);
+}
+
+pub fn add_metavar<'a>(mut mvars: Metavars<'a>, t: &'a str, def: Vec<&'a str>) -> Metavars<'a> {
+    mvars.push((t, def));
+    return mvars;
+}
