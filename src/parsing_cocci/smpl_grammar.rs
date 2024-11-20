11ceceb7c1b7e06f2d840bf23f6eccf95b1d0aa8
T Roy <t.roy@student.vu.nl>
2024-11-20 14:51:45 +0100

    Parser runs?

--- src/parsing_cocci/smpl_grammar.rs
+++ src/parsing_cocci/smpl_grammar.rs
@@ -4,9 +4,14 @@ pub type RuleType<'a> = (RuleInfo<'a>, Metavars<'a>, Vec<DisjElems>);
 pub type Metavar<'a> = (&'a str, Vec<&'a str>);
 pub type Metavars<'a> = Vec<Metavar<'a>>;
 
+/// A Mod is a string of contiguous characters that dont have any disjunction in them
+/// A DisjBranches element contains vector of posisble branches
+/// Each branch is made up of a vector of DisjElems which mean that there can be
+/// mods followed by other disjs and then more mods
+/// It is enforced during parsing that no mod are next to each other
 pub enum DisjElems {
     Mod(ModType),
-    DisjBranches(Vec<DisjElems>)
+    DisjBranches(Vec<DisjElems>),
 }
 
 // impl DisjElems {
